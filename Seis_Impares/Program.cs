53d19c499abfb11b7cc7be0a1dd05661b50c90dc
Andre <aramaldes876@gmail.com>
2020-07-11 21:13:19 -0300

    Exercicio udemy Seis Impares

---
+++ Seis_Impares/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seis_Impares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            List<int> lista = new List<int>();
+            for(int i = 0; i < 12; i++)
+            {
+                if(x % 2 != 0)
+                {
+                    lista.Add(x);
+                }
+                x++;
+            }
+            foreach(int list in lista)
+            {
+                Console.WriteLine(list);
+            }
+        }
+    }
+}
